e4aba5a679a6c9d0516881a37f10f1e223a36cb0
vishwaaniruddh <86979352+vishwaaniruddh@users.noreply.github.com>
2025-01-15 18:55:54 +0530

    category units and products done

--- api/config.php
+++ api/config.php
@@ -1,9 +1,9 @@
 <?php session_start();
 date_default_timezone_set('Asia/Kolkata');
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 $host="localhost";
