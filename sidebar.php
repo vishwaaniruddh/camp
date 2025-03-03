eb7a6a377313e605cae951a68b76c1c0894459b3
vishwaaniruddh <86979352+vishwaaniruddh@users.noreply.github.com>
2025-03-03 14:23:34 +0530

    corrected navigation

--- sidebar.php
+++ sidebar.php
@@ -34,14 +34,51 @@
 				</li>
 				<li><a href="couriers.php"><i class="fe fe-truck"></i> <span>Courier</span></a></li>
 
-				<!-- Inventory Management -->
-				<li class="menu-title"><span>Inventory</span></li>
-				<li><a href="inventory.php"><i class="fe fe-box"></i> <span>Inventory Overview</span></a></li>
-				<li><a href="incoming_request.php"><i class="fe fe-file-text"></i> <span>Material Requests</span></a></li>
+				<li class="menu-title"><span>Inventory Management</span></li>
+				<li class="submenu">
+					<a href="#"><i class="fe fe-box"></i> <span>Inventory</span> <span
+							class="menu-arrow"></span></a>
+					<ul style="display: none;">
+						<li><a href="inventory.php"><i class="fe fe-box"></i> Stock Overview</a></li>
+						<li><a href="incoming_request.php"><i class="fe fe-file-text"></i> Material Requests</a></li>
+						<li><a href="stock-adjustments.php"><i class="fe fe-refresh-cw"></i> Stock Adjustments</a></li>
+						<li><a href="stock-transfers.php"><i class="fe fe-truck"></i> Stock Transfers</a></li>
+						<li><a href="warehouses.php"><i class="fe fe-home"></i> Warehouse Management</a></li>
+						<li><a href="spare-parts.php"><i class="fe fe-tool"></i> Spare Parts Catalog</a></li>
+						<li><a href="stock-expiry.php"><i class="fe fe-clock"></i> Stock Expiry & Alerts</a></li>
+					</ul>
+				</li>
+
+				<!-- Dispatch & Tracking -->
+				<li class="submenu">
+					<a href="#"><i class="fe fe-truck"></i> <span>Dispatch & Tracking</span> <span
+							class="menu-arrow"></span></a>
+					<ul style="display: none;">
+						<li><a href="dispatch-orders.php"><i class="fe fe-send"></i> Dispatch Orders</a></li>
+						<li><a href="couriers.php"><i class="fe fe-box"></i> Courier Tracking</a></li>
+						<li><a href="live-tracking.php"><i class="fe fe-map"></i> Live GPS Tracking</a></li>
+						<li><a href="delivery-confirmations.php"><i class="fe fe-check-circle"></i> Delivery
+								Confirmations</a></li>
+					</ul>
+				</li>
+
+				<!-- Installation & Service Logs -->
+				<li class="submenu">
+					<a href="#"><i class="fe fe-list"></i> <span>Installation & Service Logs</span> <span
+							class="menu-arrow"></span></a>
+					<ul style="display: none;">
+						<li><a href="new-installations.php"><i class="fe fe-plus-circle"></i> New Installations</a></li>
+						<li><a href="service-history.php"><i class="fe fe-settings"></i> Service History</a></li>
+						<li><a href="return-management.php"><i class="fe fe-refresh-ccw"></i> Defective Part Returns</a>
+						</li>
+						<li><a href="engineer-logs.php"><i class="fe fe-user-check"></i> Engineer Work Logs</a></li>
+					</ul>
+				</li>
 
 				<!-- Purchases -->
 				<li class="menu-title"><span>Purchases</span></li>
-				<li><a href="add-purchase-orders.php"><i class="fe fe-plus-circle"></i> <span>Add Purchase Order</span></a></li>
+				<li><a href="add-purchase-orders.php"><i class="fe fe-plus-circle"></i> <span>Add Purchase
+							Order</span></a></li>
 				<li><a href="purchase-orders.php"><i class="fe fe-list"></i> <span>View Purchase Orders</span></a></li>
 				<li><a href="#"><i class="fe fe-file-text"></i> <span>Debit Notes (Coming Soon)</span></a></li>
 
@@ -51,7 +88,7 @@
 					<a href="#"><i class="fe fe-bar-chart"></i> <span>Reports</span> <span
 							class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a href="#"><i class="fe fe-file-text"></i> <span>Expense Report</span>	</a></li>
+						<li><a href="#"><i class="fe fe-file-text"></i> <span>Expense Report</span> </a></li>
 						<li><a href="#"><i class="fe fe-file-text"></i> <span>Purchase Report</span></a></li>
 						<li><a href="#"><i class="fe fe-file-text"></i> <span>Purchase Return Report</span></a></li>
 						<li><a href="#"><i class="fe fe-file-text"></i> <span>Sales Report</span></a></li>
