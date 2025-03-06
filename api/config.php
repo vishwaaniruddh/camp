6e89cbdde089963c035ef54ebeec8d108d59bf89
vishwaaniruddh <86979352+vishwaaniruddh@users.noreply.github.com>
2025-03-07 02:56:00 +0530

    added debugging

--- api/config.php
+++ api/config.php
@@ -6,9 +6,9 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Allowed 
 
 
 error_reporting(0);
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 
